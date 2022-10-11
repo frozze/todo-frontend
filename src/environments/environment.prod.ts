76e10484ef40c6981d9a5ea61197d922b7fccb44
Egor-Wexler <Egor_Wexler@dell.com>
2022-10-11 15:51:03 +0300

    Add URLs to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  getEntriesUrl: 'http://localhost:8000/api/entries/',
+  updateEntriesUrl: 'http://localhost:8000/api/save_entries/',
 };
