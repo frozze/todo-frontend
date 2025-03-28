113b88678c249ea6c165d45d0afef9c71a3411b6
Pavel Khomiakov <frozze@Mac.lan>
2025-03-28 16:22:17 +0300

    changed ip for heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  getEntriesUrl: 'http://212.34.142.171:8000/api/entries/',
-  updateEntriesUrl: 'http://212.34.142.171:8000/api/save_entries/',
-  // getEntriesUrl: 'https://egor-grocery-web-server-cbc9d33e28be.herokuapp.com/api/entries/',
-  // updateEntriesUrl: 'https://egor-grocery-web-server-cbc9d33e28be.herokuapp.com/api/save_entries/',
+  //getEntriesUrl: 'http://212.34.142.171:8000/api/entries/',
+  //updateEntriesUrl: 'http://212.34.142.171:8000/api/save_entries/',
+  getEntriesUrl: 'https://froze-todo-d550fc7f6e21.herokuapp.com//api/entries/',
+  updateEntriesUrl: 'https://froze-todo-d550fc7f6e21.herokuapp.com//api/save_entries/',
 };
